89e5385a52ee35606eb0ce75745980b1a9f8126e
Duong <duongchan159@gmail.com>
2020-04-02 09:42:52 +0700

    Added function to censor list of words

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -4,11 +4,27 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
+import re
+
 print(type(email_one))
 def censor_word( text, word_to_censor ):
-    if text.find(word_to_censor) != -1:
-        text = text.replace(word_to_censor, '**********')
+    text = re.sub(r'\b' + word_to_censor + r'\b', '******', text)
     return text
 
-email_one = censor_word(email_one, 'learning algorithms')
-print(email_one)
+#print( email_one )
+email_one = censor_word( email_one, 'learning algorithms' )
+print( email_one )
+
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithms", "her", "herself"]
+def censor_list_of_words( text, lst_to_censor ):
+    title_lst = [word.title() for word in lst_to_censor]
+    for word in lst_to_censor: 
+        text = re.sub(r'\b' + word + r'\b', '******', text) 
+    for word in title_lst:
+        text = re.sub(r'\b' + word + r'\b', '******', text)
+    return text
+
+#print( email_two )
+email_two = censor_list_of_words( email_two, proprietary_terms )
+print( email_two )
+
